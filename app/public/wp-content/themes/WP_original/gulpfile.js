085def66c1528aa4240a6b5e375b8a7e3aaf14d6
taro-ejiri <sept1976@gmail.com>
2022-04-24 16:47:10 +0900

    Gulp ベンダープレフィックス自動付与を追加

--- app/public/wp-content/themes/WP_original/gulpfile.js
+++ app/public/wp-content/themes/WP_original/gulpfile.js
@@ -2,6 +2,18 @@
 const { src, dest, watch } = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass")(require("sass"));
+// Autoprefixと一緒に使うもの
+// const postcss = require("gulp-postcss"); 
+// ベンダープレフィックス自動付与
+const autoprefixer = require('gulp-autoprefixer');
+
+//ベンダープレフィックスを付与する条件
+const TARGET_BROWSERS = [
+    'last 2 versions', // 各ブラウザの2世代前までのバージョンを担保
+    "ie >= 11", // ie11以上を担保
+    "Android >= 4", // Android4以上
+    "ios_saf >= 8" // ios safari8以上
+  ];
 
 /**
  * Sassをコンパイルするタスク
@@ -17,6 +29,7 @@ const compileSass = () =>
         outputStyle: "expanded"
       })
     )
+    .pipe(autoprefixer(TARGET_BROWSERS))
     // cssフォルダー以下に保存
     .pipe(dest("css"));
 
