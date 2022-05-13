c6b6de7039293e5bf1066aaf3b80c80db994d57b
taro-ejiri <sept1976@gmail.com>
2022-05-13 21:53:03 +0900

    Back to topButton

--- app/public/wp-content/themes/WP_original/footer.php
+++ app/public/wp-content/themes/WP_original/footer.php
@@ -1,14 +1,6 @@
 </div><!-- /.wrapper -->
-
-<!-- Back to top button -->
-<!-- <div id="app-pagetop" v-cloak>
-	<transition>
-		<div class="pagetop" v-show="scY > 200" @click="toTop" v-transition>
-			<i class="fa fa-chevron-up"></i>
-		</div>
-	</transition>
-</div> -->
-
+<!-- Back to topButton -->
+<p class="pageTop"><a href="#header"></a></p>
 <footer>
 	<div class="f_share_btn">
 		<ul>
@@ -21,67 +13,17 @@
 	</div>
 	<div class="cp_right">©2017-2022 ET WEB DESIGN</div>
 </footer>
-
-<!-- jquery -->
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 <!-- slick -->
-<!-- <script src="<?php bloginfo('template_url'); ?>/js/slick/jquery-2.2.0.min.js" type="text/javascript" charset="utf-8"></script> -->
 <script src="<?php bloginfo('template_url'); ?>/js/slick/slick.min.js" type="text/javascript" charset="utf-8"></script>
-<script type="text/javascript">
-	// text-slider
-	$('.text-slides').slick({
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed:4500,
-		fade: true,
-	});
-	// front-slider
-	$('.front-slick').slick({
-		arrows: true,
-		autoplay:true,
-		autoplaySpeed:3000,
-		dots:true
-	});
-	// works-slider
-	$('.works-slider').slick({
-		arrows: false,
-		dots:true
-	});
-</script>
-<!-- works並び替え --><!-- 投稿タイプが「works」のアーカイブページで行う処理を書く-->
+<!-- ShareButton -->
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.easy-rollover.min.js"></script>
+<!-- works sort -->
 <?php if ( is_post_type_archive('works') ) : ?>
-	<!-- <script src="<?php bloginfo('template_url'); ?>/js/works/jquery-2.1.4.min.js"></script> -->
 	<script src="<?php bloginfo('template_url'); ?>/js/works/masonry.pkgd.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/works/extention.js"></script>
 <?php endif; ?>
-<!-- シェアボタン js -->
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.easy-rollover.min.js"></script>
-<script type="text/javascript">
-	$(function () {
-		//シェアボタン　マウスオーバー挙動
-		$('img').easyRollover();
-	});
-</script>
-<!-- ページ移動の際の表示設定 -->
-<script type="text/javascript">
-	$(window).on('load', function () {
-		$('body').removeClass('fadeout');
-	});
-	$(function () {
-		// ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
-		$('a:not([href^="#"]):not([target])').on('click', function (e) {
-			e.preventDefault(); // ナビゲートをキャンセル
-			url = $(this).attr('href'); // 遷移先のURLを取得
-			if (url !== '') {
-				$('body').addClass('fadeout'); // bodyに class="fadeout"を挿入
-				setTimeout(function () {
-					window.location = url; // 0.8秒後に取得したURLに遷移
-				}, 800);
-			}
-			return false;
-		});
-	});
-</script>
 <?php wp_footer(); ?>
 </body>
 </html>
---
+++ app/public/wp-content/themes/WP_original/js/script.js
@@ -0,0 +1,61 @@
+
+// page fadeout
+$(window).on('load', function () {
+    $('body').removeClass('fadeout');
+});
+$(function() {
+    // page fadeout ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+    $('a:not([href^="#"]):not([target])').on('click', function (e) {
+        e.preventDefault(); // ナビゲートをキャンセル
+        url = $(this).attr('href'); // 遷移先のURLを取得
+        if (url !== '') {
+            $('body').addClass('fadeout'); // bodyに"fadeout"を挿入
+            setTimeout(function () {
+                window.location = url; // 0.8秒後に遷移
+            }, 800);
+        }
+        return false;
+    });
+
+    // slick
+	$('.text-slides').slick({
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed:4500,
+		fade: true,
+	});
+	$('.front-slick').slick({
+		arrows: true,
+		autoplay:true,
+		autoplaySpeed:3000,
+		dots:true
+	});
+	$('.works-slider').slick({
+		arrows: false,
+		dots:true
+	});
+
+    // ShareButton
+    $('img').easyRollover();
+
+    // Back to topButton
+	var pageTopBtn = $('.pageTop');	
+	pageTopBtn.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			pageTopBtn.fadeIn();
+		} else {
+			pageTopBtn.fadeOut();
+		}
+	});
+	pageTopBtn.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		return false;
+	});
+
+    // ブラウザリロード時にスクロールをページトップ
+    $('html,body').animate({ scrollTop: 0 }, '1');
+});
+
