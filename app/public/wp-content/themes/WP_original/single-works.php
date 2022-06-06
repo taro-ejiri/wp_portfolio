9dbdff86559ce1b332769ef2129b5e585c660973
taro-ejiri <sept1976@gmail.com>
2022-06-06 18:29:24 +0900

    single-works改修中

--- app/public/wp-content/themes/WP_original/single-works.php
+++ app/public/wp-content/themes/WP_original/single-works.php
@@ -19,7 +19,8 @@
 		<!-- data-interval="3000"を入れると切り替わるタイミングが変わる（3000で3秒） -->
 		<div id="carousel" class="carousel slide" data-ride="carousel" data-interval="10000">
 			<?php // PCサイズしかないサイトの場合 
-				$ctm = get_post_meta($post->ID, 'slideImage_pc', true);?>
+				$ctm = get_post_meta($post->ID, 'slideImage_1', true);?>
+				<!-- $ctm = get_post_meta($post->ID, 'slideImage_pc', true);?> -->
 			<?php if(empty($ctm)):?>
 				<div class="carousel-inner">
 					<?php 
@@ -38,126 +39,71 @@
 					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
 				<?php endif; ?>
 			<?php else: // 通常 ?>
-
-
-
-
-
 				<ul class="slider works-slider">
-				<?php if(get_field('slideImage_pc')): ?>
-				<li>
-						<?php //写真設定
-							$image = get_field('slideImage_pc');
-							$alt = $image['alt'];
-							$url = $image['sizes']['large'];
-						?>
-						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-						<div class="size">PCサイズ</div>
-				</li>
-				<?php endif; ?>
-
-				<?php if(get_field('slideImage_1')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_1');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_1'); ?>" target="blank"><?php the_field('siteName_1'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-
-				<?php if(get_field('slideImage_2')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_2');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_2'); ?>" target="blank"><?php the_field('siteName_2'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-				<?php if(get_field('slideImage_3')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_3');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_3'); ?>" target="blank"><?php the_field('siteName_3'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-				<?php if(get_field('slideImage_4')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_4');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_4'); ?>" target="blank"><?php the_field('siteName_4'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-				<?php if(get_field('slideImage_5')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_5');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_5'); ?>" target="blank"><?php the_field('siteName_5'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-				<?php if(get_field('slideImage_6')): ?>
-				<li>
-					<?php //写真設定
-						$image = get_field('slideImage_6');
-						$alt = $image['alt'];
-						$url = $image['sizes']['large'];
-					?>
-					<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-					<div class="size"><a href="<?php the_field('siteURL_6'); ?>" target="blank"><?php the_field('siteName_6'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
-				</li>
-				<?php endif; ?>
-
-
-
-
-
-
-					<!-- <li>
-						<?php //写真設定
-							$image = get_field('slideImage_pc');
-							$alt = $image['alt'];
-							$url = $image['sizes']['large'];
-						?>
-						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-						<div class="size">PCサイズ</div>
-					</li> -->
-					<?php if(get_field('slideImage_tab')): ?>
+					<?php if(get_field('slideImage_1')): ?>
 					<li>
 						<?php //写真設定
-							$image = get_field('slideImage_tab');
+							$image = get_field('slideImage_1');
 							$alt = $image['alt'];
 							$url = $image['sizes']['large'];
 						?>
 						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-						<div class="size">タブレットサイズ</div>
+						<div class="size"><a href="<?php the_field('siteURL_1'); ?>" target="blank"><?php the_field('siteName_1'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
 					</li>
 					<?php endif; ?>
-					<?php if(get_field('slideImage_sp')): ?>
+					<?php if(get_field('slideImage_2')): ?>
 					<li>
 						<?php //写真設定
-							$image = get_field('slideImage_sp');
+							$image = get_field('slideImage_2');
 							$alt = $image['alt'];
 							$url = $image['sizes']['large'];
 						?>
 						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
-						<div class="size">スマートフォンサイズ</div>
+						<div class="size"><a href="<?php the_field('siteURL_2'); ?>" target="blank"><?php the_field('siteName_2'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
+					</li>
+					<?php endif; ?>
+					<?php if(get_field('slideImage_3')): ?>
+					<li>
+						<?php //写真設定
+							$image = get_field('slideImage_3');
+							$alt = $image['alt'];
+							$url = $image['sizes']['large'];
+						?>
+						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
+						<div class="size"><a href="<?php the_field('siteURL_3'); ?>" target="blank"><?php the_field('siteName_3'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
+					</li>
+					<?php endif; ?>
+					<?php if(get_field('slideImage_4')): ?>
+					<li>
+						<?php //写真設定
+							$image = get_field('slideImage_4');
+							$alt = $image['alt'];
+							$url = $image['sizes']['large'];
+						?>
+						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
+						<div class="size"><a href="<?php the_field('siteURL_4'); ?>" target="blank"><?php the_field('siteName_4'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
+					</li>
+					<?php endif; ?>
+					<?php if(get_field('slideImage_5')): ?>
+					<li>
+						<?php //写真設定
+							$image = get_field('slideImage_5');
+							$alt = $image['alt'];
+							$url = $image['sizes']['large'];
+						?>
+						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
+						<div class="size"><a href="<?php the_field('siteURL_5'); ?>" target="blank"><?php the_field('siteName_5'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
+					</li>
+					<?php endif; ?>
+					<?php if(get_field('slideImage_6')): ?>
+					<li>
+						<?php //写真設定
+							$image = get_field('slideImage_6');
+							$alt = $image['alt'];
+							$url = $image['sizes']['large'];
+						?>
+						<img src="<?php echo $url ?>" alt="<?php echo $alt ?>" class="img-responsive"/>
+						<div class="size"><a href="<?php the_field('siteURL_6'); ?>" target="blank"><?php the_field('siteName_6'); ?><i class="fa fa-external-link" aria-hidden="true"></i></a></div>
 					</li>
 					<?php endif; ?>
 				</ul>
@@ -166,22 +112,17 @@
 	</div>
 	<div class="Works-comment">
 		<div class="comment">
-			<!-- 概要欄 -->
 			<p><?php the_field('column'); ?></p>
 		</div>
 		<dl class="dl-center">
-			<!-- 使用ツール -->
-			<dt>TOOL:</dt>
+			<dt>LANG / TOOL:</dt>
 			<dd><?php the_field('tool'); ?></dd>
-			<!-- 作業範囲 -->
 			<dt>SCOPE:</dt>
 			<dd><?php the_field('scope'); ?></dd>
-			<!-- クライアント名（日本語）-->
 			<dt>CLIENT:</dt>
 			<dd><?php the_field('client_jpn'); ?>様</dd>
-			<!-- サイトURL -->
-			<dt>SITE:</dt>
-			<dd><a href="<?php the_field('siteURL'); ?>" target="blank"><?php the_field('siteURL'); ?> <i class="fa fa-external-link" aria-hidden="true"></i></a></dd>
+			<!-- <dt>SITE:</dt>
+			<dd><a href="<?php the_field('siteURL'); ?>" target="blank"><?php the_field('siteURL'); ?> <i class="fa fa-external-link" aria-hidden="true"></i></a></dd> -->
 		</dl>
 	</div>
 <?php endwhile; ?>
